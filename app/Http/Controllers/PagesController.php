53c79cdb6229808971076c66242edaff59d996a6
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-26 19:08:17 +0200

    using controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,19 +5,19 @@ namespace App\Http\Controllers;
 class PagesController extends Controller {
 
   public function getIndex(){
-
+    
   }
 
   public function getAbout(){
-
+    return "About Me!";
   }
 
   public function getContact(){
-
+    return "Hello Contact Page";
   }
 
   public function postContact(){
-    
+
   }
 
 }
