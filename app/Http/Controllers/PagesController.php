fff88bb1960ed1c1327061f995fdb3810fad4d56
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-26 18:45:48 +0200

    pages controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller {
+
+  public function getIndex(){
+
+  }
+
+  public function getAbout(){
+
+  }
+
+  public function getContact(){
+
+  }
+
+  public function postContact(){
+    
+  }
+
+}
+ ?>
