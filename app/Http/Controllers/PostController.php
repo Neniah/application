ec96cbba4f2036bbe92d16999df1a66d109daf0a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-27 20:11:28 +0200

    create a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use App\Post;
+use Session;
 
 class PostController extends Controller
 {
@@ -26,7 +28,6 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
         return view('posts.create');
     }
 
@@ -40,16 +41,20 @@ class PostController extends Controller
     {
         // validate the data
         $this->validate($request, array(
-          'title' => 'required|max:255',
-          'body' => 'required'
-        ));
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
+
         // store in the database
         $post = new Post;
+
         $post->title = $request->title;
         $post->body = $request->body;
 
-        $post-> save();
-        // redirect to another page
+        $post->save();
+
+      //  Session::flash('success', 'The blog post was successfully save!');
+
         return redirect()->route('posts.show', $post->id);
     }
 
@@ -61,7 +66,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('posts.show');
     }
 
     /**
