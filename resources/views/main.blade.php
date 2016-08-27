fad918ad2604aa441719574e8dbb878925a8b207
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-27 13:43:47 +0200

    partials and migration

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,19 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
 @include('partials._head')
+
   <body>
-@include('partials._nav')
 
+  @include('partials._nav')
 
+  <div class="container">
 
-    <div class="container">
+    @yield('content')
 
-      @yield('content')
+    @include('partials._footer')
 
-@include('partials._footer')
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    @include('partials._javascript')
 
     @yield('scripts')
   </body>
