c5defd65724791a7e8ccc17316b4c157d14bf8bb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-27 18:49:09 +0200

    a form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,6 +8,16 @@
     <div class="col-md-8 col-md-offset-2">
       <h1>Create New Post</h1>
       <hr>
+      {!! Form::open(array('url' => 'posts.store')) !!}
+        {{ Form::label('title', 'Title: ') }}
+        {{ Form::text('title', null, array('class' => 'form-control')) }}
+
+        {{ Form::label('body', 'Post Body: ') }}
+        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+
+        {{ Form::submit('Create Post', array('class' => 'btn btn-primary btn-lg btn-block')) }}
+
+      {!! Form::close() !!}
     </div>
   </div>
 
