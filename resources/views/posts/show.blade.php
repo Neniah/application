dffe085ba05fd13061fdab86d5149a57be85f8de
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-31 09:36:43 +0200

    dates

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,35 @@
 
 @section('content')
 
-  <h1>{{ $post->title }}</h1>
+  <div class="row">
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
 
-  <p class="lead">
-    {{ $post->body }}
-  </p>
+      <p class="lead">
+        {{ $post->body }}
+      </p>
+      <div class="col-md-8">
+        <div class="well">
+          <dl class="dl-horizontal">
+            <dt>Create At:</dt>
+            <dd>{{ date('M d Y', strtotime($post->created_at)) }}</dd>
+          </dl>
+          <dl class="dl-horizontal">
+            <dt>Last Updated:</dt>
+            <dd>{{ date('M d Y', strtotime($post->updated_at)) }}</dd>
+          </dl>
+          <hr>
+          <div class="row">
+            <div class="col-sm-6">
+              <a href="#" class="btn btn-primary btn-block">Edit</a>
+            </div>
+            <div class="col-sm-6">
+              <a href="#" class="btn btn-danger btn-block">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
