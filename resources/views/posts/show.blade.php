d79b178034373fc6d63f97cbaaecbe57d9ec2179
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-31 10:09:16 +0200

    setting dates

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,19 +15,19 @@
         <div class="well">
           <dl class="dl-horizontal">
             <dt>Create At:</dt>
-            <dd>{{ date('M d Y', strtotime($post->created_at)) }}</dd>
+            <dd>{{ date('M d, Y h:ia', strtotime($post->created_at)) }}</dd>
           </dl>
           <dl class="dl-horizontal">
             <dt>Last Updated:</dt>
-            <dd>{{ date('M d Y', strtotime($post->updated_at)) }}</dd>
+            <dd>{{ date('M d, Y h:ia', strtotime($post->updated_at)) }}</dd>
           </dl>
           <hr>
           <div class="row">
             <div class="col-sm-6">
-              <a href="#" class="btn btn-primary btn-block">Edit</a>
+              {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block') )!!}
             </div>
             <div class="col-sm-6">
-              <a href="#" class="btn btn-danger btn-block">Delete</a>
+            {!! Html::linkRoute('posts.edit', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block') )!!}
             </div>
           </div>
         </div>
