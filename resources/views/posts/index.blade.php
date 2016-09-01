518f4a14c0c8097430688afec7a7559b120e08fe
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-01 21:06:37 +0200

    a table for info

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,6 +12,54 @@
     <div class="col-md-2">
       <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary btn-h1-spacing">Create New Post</a>
     </div>
+
+    <div class="col-md-12">
+      <hr>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table">
+          <thead>
+            <th>#</th>
+            <th>
+              Title
+            </th>
+            <th>
+              Body
+            </th>
+            <th>
+              Created At
+            </th>
+            <td>
+            </td>
+          </thead>
+          <tbody>
+            @foreach($posts as $post)
+              <tr>
+                <th>
+                  {{ $post->id }}
+                </th>
+                <td>
+                  {{ $post->title }}
+                </td>
+                <td>
+                  $post->body
+                </td>
+                <td>
+                  $post->created_at
+                </td>
+                <td>
+                  <a href="#" class="btn btn-default">View</a>
+                  <a href="#" class="btn btn-default">Edit</a>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
     <hr>
   </div>
 
