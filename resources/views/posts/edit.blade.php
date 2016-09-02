1b32b0d01cef04944672c7b993524bf852d4611a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-02 12:28:13 +0200

    edit view

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('main')
+
+@section('title', '| Edit Blog Post')
+
+@section('content')
+
+  <div class="row">
+    {!! Form::open() !!}
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
+
+      <p class="lead">
+        {{ $post->body }}
+      </p>
+      <div class="col-md-8">
+        <div class="well">
+          <dl class="dl-horizontal">
+            <dt>Create At:</dt>
+            <dd>{{ date('M d, Y h:ia', strtotime($post->created_at)) }}</dd>
+          </dl>
+          <dl class="dl-horizontal">
+            <dt>Last Updated:</dt>
+            <dd>{{ date('M d, Y h:ia', strtotime($post->updated_at)) }}</dd>
+          </dl>
+          <hr>
+          <div class="row">
+            <div class="col-sm-6">
+              {!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block') )!!}
+            </div>
+            <div class="col-sm-6">
+            {!! Html::linkRoute('posts.update', 'Save Changes', array($post->id), array('class' => 'btn btn-success btn-block') )!!}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+@stop
