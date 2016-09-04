aa5a983d7f920586c18c11ac374af17682f7f354
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-04 19:43:39 +0200

    pagination links

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,16 +37,10 @@
           <tbody>
             @foreach($posts as $post)
               <tr>
-                <th>
-                  {{ $post->id }}
-                </th>
-                <td>
-                  {{ $post->title }}
-                </td>
+                <th>{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
                 <td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
-                <td>
-                  {{ date('M j, Y', strtotime($post->created_at)) }}
-                </td>
+                <td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
                 <td>
                   <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a>
                   <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
@@ -55,6 +49,9 @@
             @endforeach
           </tbody>
         </table>
+        <div class="text-center">
+          {!! $posts->links(); !!}
+        </div>
       </div>
     </div>
 
