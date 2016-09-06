7c09ffc4b6f1a0e997b2827fd9a53453924708ab
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-06 18:59:13 +0200

    single view

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
 
 class BlogController extends Controller
 {
     //
 
     public function getSingle($slug){
-      return $slug;
+      $post = Post::where('slug', '=', $slug)->first();
+
+      return view('blog.single')->withPost($post);
     }
 }
