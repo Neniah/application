ed0c6d72afac465b6a20733fc2517c3464981e47
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-06 12:31:31 +0200

    slug on routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-
+Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle']);
 Route::get('/', 'PagesController@getIndex');
 Route::get('/about', 'PagesController@getAbout');
 Route::get('contact', 'PagesController@getContact');
