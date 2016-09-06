1f3f49bc4d43106fd8acefa378f4cdb59a9ddc6b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-06 19:32:56 +0200

    post title on slug page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])
-  ->where('slug', '[\w\d\-\_]');
+Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('/', 'PagesController@getIndex');
 Route::get('/about', 'PagesController@getAbout');
 Route::get('contact', 'PagesController@getContact');
