0e19264658e09cfea5ff3fa54f2ccefb9346f577
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-09 11:40:12 +0200

    auth::check

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -23,7 +23,8 @@
       <ul class="nav navbar-nav navbar-right">
         @if(Auth::check())
         <li class="dropdown">
-          <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+          <a href="/" class="dropdown-toggle" data-toggle="dropdown" r
+          ole="button" aria-haspopup="true" aria-expanded="false">Hello {{ Auth::user()->name }} <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="{{route('posts.index')}}">Posts</a></li>
             <li><a href="#">Another action</a></li>
