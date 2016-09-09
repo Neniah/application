f9da9ada2e87f06149b42b0f8b0e5e6a7799a568
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-09 13:22:53 +0200

    views for rest password

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,31 @@
+@extends('main')
+
+@section('title', '| Forgot my Password')
+
+@section('content')
+
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">Reset Password</div>
+        <div class="panel-body">
+          {!! Form::open(['url' => 'password/reset', 'method' => 'POST']) !!}
+
+          {{ Form::label('email', 'Email Address:') }}
+          {{ Form::email('email', null, ['class' => 'form-control']) }}
+
+          {{ Form::label('password', 'New Password:') }}
+          {{ Form::password('password', ['class' => 'form-control']) }}
+
+          {{ Form::label('password_confirmation', 'Confirmed New Password') }}
+          {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+
+          {{ Form::submit('Reset Password', ['class' => 'btn btn-primary']) }}
+
+          {!! Form::close() !!}
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
