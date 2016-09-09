12a71e2f2cf1ac869cc58e11c1a572e0ced1ce2c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-09 11:15:33 +0200

    routes name, logout route in nav

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,7 +3,7 @@
 //Authentication Routes
 Route::get('auth/login', [ 'as' => 'login', 'uses' => 'Auth\AuthController@getLogin']);
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('auth/logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
 
 //Registration routes
 Route::get('auth/register', 'Auth\AuthController@getRegister');
