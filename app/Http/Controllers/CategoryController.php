482d580a8c66328a1b272d466d7a755cecc503bd
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-10 18:42:52 +0200

    category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CategoryController extends Controller
+{
+    //
+    public function __contruct(){
+      $this->middleware('auth');
+    }
+
+    public function index(){
+
+    }
+
+    public function store(){
+
+    }
+
+
+}
