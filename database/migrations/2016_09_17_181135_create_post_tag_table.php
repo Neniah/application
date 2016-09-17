7eb2e5b5f02ad4b20bfde053d6131b7b402acc21
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-17 20:12:14 +0200

    php artisan make:migration create_post_tag_table

--- database/migrations/2016_09_17_181135_create_post_tag_table.php
+++ database/migrations/2016_09_17_181135_create_post_tag_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagsTable extends Migration
+class CreatePostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,7 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -26,6 +22,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('tags');
+        //
     }
 }
