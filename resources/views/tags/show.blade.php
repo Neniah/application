44f640b833b3617b6863b481309d198564a8a1cb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-03 18:34:53 +0200

    a table...

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -9,7 +9,39 @@
         <h1>{{ $tag->name }} Tag <small>{{$tag->posts->count()}}</small></h1>
       </div>
       <div class="col-md-4">
-        <a href="#" class="btn btn-lg btn-primary pull-right">Edit</a>
+        <a href="#" class="btn btn-primary pull-right btn-block">Edit</a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>
+                #
+              </th>
+              <th>
+                Title
+              </th>
+              <th>
+                Tags
+              </th>
+              <th>
+
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($tag->posts as $post)
+              <tr>
+                <th>{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                  <td>{{ $post->title }}</td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
       </div>
     </div>
 
