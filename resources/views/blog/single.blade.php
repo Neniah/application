4799818bf9ce4533da6a1063f94b740f05990138
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-10 18:01:30 +0200

    form for send a comment on single post

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -18,6 +18,22 @@
     </div>
   </div>
 
-  <div class="row"></div>
+  <div class="row">
+    <div id="comment-form">
+      {{ Form::open(['route' => ['comments.store', $post->id], 'method' => 'POST']) }}
+        <div class="row">
+          <div class="col-md-6">
+            {{ Form::label('name', 'Name:') }}
+            {{ Form::text('name', null, ['class' => 'form-control']) }}
+          </div>
+
+          <div class="col-md-6">
+            {{ Form::label('email', 'Email:') }}
+            {{ Form::text('email', null, ['class' => 'form-control']) }}
+          </div>
+        </div>
+      {{ Form::close() }}
+    </div>
+  </div>
 
 @endsection
