2fe6cba130675d1cd3e0d9b24c663f675ba11594
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-10 18:12:41 +0200

    update comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -1,55 +1,40 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
+use App\Comment;
+use App\Post;
+use Session;
 class CommentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth', ['except' => 'store']);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $post_id)
     {
-        //
+        $this->validate($request, array(
+            'name'      =>  'required|max:255',
+            'email'     =>  'required|email|max:255',
+            'comment'   =>  'required|min:5|max:2000'
+            ));
+        $post = Post::find($post_id);
+        $comment = new Comment();
+        $comment->name = $request->name;
+        $comment->email = $request->email;
+        $comment->comment = $request->comment;
+        $comment->approved = true;
+        $comment->post()->associate($post);
+        $comment->save();
+        Session::flash('success', 'Comment was added');
+        return redirect()->route('blog.single', [$post->slug]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -58,9 +43,9 @@ class CommentsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comment = Comment::find($id);
+        return view('comments.edit')->withComment($comment);
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -70,9 +55,18 @@ class CommentsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comment = Comment::find($id);
+        $this->validate($request, array('comment' => 'required'));
+        $comment->comment = $request->comment;
+        $comment->save();
+        Session::flash('success', 'Comment updated');
+        return redirect()->route('posts.show', $comment->post->id);
     }
-
+    public function delete($id)
+    {
+        $comment = Comment::find($id);
+        return view('comments.delete')->withComment($comment);
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -81,6 +75,10 @@ class CommentsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comment = Comment::find($id);
+        $post_id = $comment->post->id;
+        $comment->delete();
+        Session::flash('success', 'Deleted Comment');
+        return redirect()->route('posts.show', $post_id);
     }
 }
